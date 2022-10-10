40797e4dd655106b04284779dbee8efc2367a80d
Sz-Tim <tim.szewczyk@gmail.com>
2022-10-10 11:18:55 +0100

    processing element activity, particle locations

--- code/02_processSimulations.R
+++ code/02_processSimulations.R
@@ -6,8 +6,6 @@
 # This script processes and summarises the simulation output
 
 
-
-
 # setup -------------------------------------------------------------------
 
 library(tidyverse); library(glue); library(lubridate); library(sf)
@@ -26,16 +24,54 @@ dirs <- switch(get_os(),
 
 # load files --------------------------------------------------------------
 
-sim_i <- read_csv(glue("{dirs$out}/sim_i.csv"))
+mesh.sf <- list(linnhe7=st_read(glue("{dirs$mesh}/linnhe_mesh.gpkg")) %>%
+                  mutate(mesh="linnhe7"),
+                westcoms2=st_read(glue("{dirs$mesh}/WeStCOMS2_linnhe_mesh.gpkg")) %>%
+                  mutate(mesh="WeStCOMS2")) %>%
+  do.call('rbind', .) %>%
+  select(i, area, depth, mesh, geom)
+sim_i <- read_csv(glue("{dirs$out}/sim_i.csv")) %>%
+  mutate(liceSpeedF=factor(liceSpeed, labels=c("Passive", "Slow", "Medium", "Fast")))
 sim_seq <- 1:nrow(sim_i)
-elemAct.df <- map_dfr(sim_seq, ~glue("{sim_i$outDir[.x]}/elementActivity.dat") %>% 
+
+
+
+
+# element activity --------------------------------------------------------
+
+elemAct.df <- map_dfr(sim_seq,
+                      ~glue("{sim_i$outDir[.x]}/elementActivity.dat") %>% 
                         read_delim(col_names=c("sink", "swim", "float", "total")) %>%
                         mutate(total=sink+swim+float,
                                i=row_number(),
-                               sim=.x))
+                               sim=.x)) %>%
+  left_join(sim_i %>% mutate(sim=as.numeric(i)) %>%
+              select(mesh, timeRes, liceSpeed, liceSpeedF, sim))
+elemAct.sf <- left_join(mesh.sf, elemAct.df, by=c("mesh", "i"))
+write_sf(elemAct.sf, "out/00_processed/elementActivity.gpkg")
 
 
-# elemActivity <- read_delim(glue("{out.dir}elementActivity.dat"), 
-#                            col_names=c("sink", "swim", "float", "total")) %>%
-#   mutate(total=sink+swim+float,
-#          i=row_number())
+
+# particle locations ------------------------------------------------------
+
+
+loc.df <- map_dfr(
+  sim_seq, 
+  ~dir(glue("{sim_i$outDir[.x]}locs"), "locations_", full.names=T) %>%
+    map_dfr(~read_delim(.x, delim=" ", col_names=T, show_col_types=F) %>%
+              select(-mesh) %>%
+              mutate(date=ymd(str_sub(.x,-12,-5)),
+                     dateTime=as_datetime(paste0(str_sub(.x,-12,-5), "-", hour),
+                                          format="%Y%m%d-%H"))) %>%
+    mutate(sim=.x)
+  ) %>%
+  left_join(sim_i %>% mutate(sim=as.numeric(i)) %>%
+              select(mesh, timeRes, liceSpeed, liceSpeedF, sim))
+write_csv(loc.df, "out/00_processed/locations.csv")
+
+
+
+
+
+
+
