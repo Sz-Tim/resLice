366812cf5e6831905317638e0d358e69f0460201
Sz-Tim <tim.szewczyk@gmail.com>
2022-10-12 11:11:13 +0100

    velocities with up/downloch direction

--- code/03_outputEDA.R
+++ code/03_outputEDA.R
@@ -27,12 +27,12 @@ dirs <- switch(get_os(),
 mesh.sf <- list(linnhe7=st_read(glue("{dirs$mesh}/linnhe_mesh.gpkg")) %>%
                   mutate(mesh="linnhe7"),
                 westcoms2=st_read(glue("{dirs$mesh}/WeStCOMS2_linnhe_mesh.gpkg")) %>%
-                  mutate(mesh="WeStCOMS2")) %>%
+                  mutate(mesh="WeStCOMS2", lochRegion=NA)) %>%
   do.call('rbind', .) %>%
-  select(i, area, depth, mesh, geom)
+  select(i, area, depth, mesh, lochRegion, geom)
 mesh.fp <- st_read("data/linnhe_mesh_footprint.gpkg")
 sim_i <- read_csv(glue("{dirs$out}/sim_i.csv")) %>%
-  mutate(liceSpeedF=factor(liceSpeed, labels=c("Passive", "Slow", "Medium", "Fast")))
+  mutate(liceSpeedF=factor(liceSpeed, labels=c("Passive", "Medium")))
 elemAct.sf <- st_read("out/00_processed/elementActivity.gpkg") %>%
   mutate(liceSpeedF=factor(liceSpeedF, levels=levels(sim_i$liceSpeedF)))
 loc.df <- read_csv("out/00_processed/locations.csv") %>%
@@ -59,7 +59,7 @@ loc.df %>% filter(status==2) %>%
   ggplot(aes(depth, colour=mesh)) +
   geom_density() +
   xlim(0, 25) +
-  facet_grid(liceSpeed~.)
+  facet_grid(liceSpeedF~.)
 
 loc.df %>% filter(status==2) %>%
   ggplot(aes(depth, colour=liceSpeedF)) +
@@ -155,11 +155,11 @@ loc.df %>%
   mutate(xy_cumul=cumsum(xy)) %>%
   filter(status==2) %>%
   slice_tail(n=1) %>%
-  ggplot(aes(xy_cumul/1e3, linetype=mesh, colour=liceSpeedF, group=sim)) + 
+  ggplot(aes(xy_cumul/age, linetype=mesh, colour=liceSpeedF, group=sim)) + 
   geom_density() + 
   scale_colour_viridis_d() +
-  scale_x_log10("Total xy hourly displacement (km)")
-ggsave("figs/xy_total_log.png", width=6, height=4, dpi=300)
+  scale_x_log10("Mean xy hourly displacement (m/h)")
+ggsave("figs/xy_mean_log.png", width=6, height=4, dpi=300)
 loc.df %>% 
   group_by(ID, sim, mesh, liceSpeedF) %>%
   arrange(age) %>%
@@ -168,32 +168,32 @@ loc.df %>%
   mutate(xy_cumul=cumsum(xy)) %>%
   filter(status==2) %>%
   slice_tail(n=1) %>%
-  ggplot(aes(xy_cumul/1e3, linetype=mesh, colour=liceSpeedF, group=sim)) + 
+  ggplot(aes(xy_cumul/age, linetype=mesh, colour=liceSpeedF, group=sim)) + 
   geom_density() + 
   scale_colour_viridis_d() +
-  scale_x_continuous("Total xy hourly displacement (km)")
-ggsave("figs/xy_total.png", width=6, height=4, dpi=300)
+  scale_x_continuous("Mean xy hourly displacement (m/h)")
+ggsave("figs/xy_mean.png", width=6, height=4, dpi=300)
 
 loc.df %>%
   group_by(ID, sim, mesh, liceSpeedF) %>%
   arrange(age) %>%
   filter(status==2) %>%
   slice_tail(n=1) %>%
-  ggplot(aes(zTot, linetype=mesh, colour=liceSpeedF, group=sim)) + 
+  ggplot(aes(zTot/age, linetype=mesh, colour=liceSpeedF, group=sim)) + 
   geom_density() + 
   scale_colour_viridis_d() +
-  scale_x_log10("Total z hourly displacement (m)")
-ggsave("figs/z_total_log.png", width=6, height=4, dpi=300)
+  scale_x_log10("Mean z hourly displacement (m)")
+ggsave("figs/z_mean_log.png", width=6, height=4, dpi=300)
 loc.df %>%
   group_by(ID, sim, mesh, liceSpeedF) %>%
   arrange(age) %>%
   filter(status==2) %>%
   slice_tail(n=1) %>%
-  ggplot(aes(zTot, linetype=mesh, colour=liceSpeedF, group=sim)) + 
+  ggplot(aes(zTot/age, linetype=mesh, colour=liceSpeedF, group=sim)) + 
   geom_density() + 
   scale_colour_viridis_d() +
-  scale_x_continuous("Total z hourly displacement (m)")
-ggsave("figs/z_total.png", width=6, height=4, dpi=300)
+  scale_x_continuous("Mean z hourly displacement (m)")
+ggsave("figs/z_mean.png", width=6, height=4, dpi=300)
 
 
 
@@ -234,3 +234,129 @@ for(i in 1:nrow(sim_i)) {
          p, width=6, height=6, dpi=900)
 }
 
+
+
+
+
+
+
+# velocities --------------------------------------------------------------
+
+velocity.df <- loc.df %>%
+  select(ID, dateTime, age, status, x, y, sim, mesh, elem, density, depth) %>%
+  arrange(sim, dateTime, ID) %>%
+  group_by(ID, sim) %>%
+  mutate(x_m1=lag(x), 
+         y_m1=lag(y),
+         depth_m1=lag(depth)) %>%
+  ungroup %>%
+  mutate(dx=x-x_m1,
+         dy=y-y_m1,
+         dz=depth-depth_m1,
+         dXY=sqrt(dx^2 + dy^2),
+         bearing=atan2(dy, dx)) %>%
+  filter(status==2)
+
+
+velocity.df %>%
+  filter(sim==2) %>%
+  filter(ID %% 100 == 0) %>%
+  ggplot(aes(x, y, colour=bearing)) +
+  geom_point(size=0.5) +
+  scale_colour_gradientn(colours=cmr$infinity, breaks=c(-pi, -pi/2, 0, pi/2, pi),
+                         labels=c("W", "S", "E", "N", "W"), limits=c(-pi, pi))
+
+# uploch (from mouth to head, but confirm) bearing in radians
+lochDir.df <- st_read("d:/hydroOut/linnhe_skeleton.shp") %>%
+  mutate(x=st_coordinates(.)[,1],
+         y=st_coordinates(.)[,2]) %>%
+  st_drop_geometry() %>%
+  arrange(lochRegion, lochHead) %>%
+  group_by(lochRegion) %>%
+  summarise(bearing=atan2(first(y)-last(y), first(x)-last(x)))
+mesh.sf <- mesh.sf %>%
+  full_join(lochDir.df, by="lochRegion")
+velocity.df <- velocity.df %>%
+  filter(mesh=="linnhe7") %>%
+  left_join(., mesh.sf %>% st_drop_geometry() %>% 
+              select(i, mesh, bearing, depth) %>% 
+              rename(elem=i, lochDir=bearing, meshDepth=depth),
+            by=c("mesh", "elem")) %>%
+  mutate(downloch=cos(lochDir-bearing)) # -1: uploch, 1: downloch
+# cos(lochDir - partDir)
+# positive = towards same direction (aligned at 1)
+# negative = opposite direction (aligned at -1)
+
+velocity.df %>%
+  filter(sim==4) %>%
+  filter(ID %% 100 == 0) %>%
+  ggplot(aes(x, y, colour=downloch)) +
+  geom_point() +
+  scale_colour_gradient2("Direction", limits=c(-1,1), 
+                         breaks=c(-1,1), labels=c("uploch", "downloch")) +
+  facet_wrap(~hour(dateTime), ncol=6)
+
+velocity.df %>%
+  mutate(depthCat=if_else(depth < 20, round(depth), 20)) %>%
+  ggplot(aes(downloch, colour=depthCat, group=depthCat)) +
+  geom_density() + 
+  scale_colour_viridis_c(direction=-1) +
+  facet_grid(~sim)
+
+
+# compare across depths: absolute, or relative to meshDepth
+velocity.df %>%
+  mutate(depthCat=if_else(depth < 20, round(depth), 20)) %>%
+  ggplot(aes(downloch, colour=depthCat, group=depthCat)) +
+  geom_density() + 
+  scale_colour_viridis_c(direction=-1) +
+  facet_grid(~sim)
+velocity.df %>%
+  mutate(depthCat=if_else(depth<=5, "<= 5m", "> 5m")) %>%
+  ggplot(aes(downloch, colour=depthCat, group=depthCat)) +
+  geom_density() + 
+  facet_grid(~sim)
+velocity.df %>%
+  mutate(depthCat=if_else(depth<=5, "<= 5m", "> 5m")) %>%
+  ggplot(aes(downloch, colour=hour(dateTime), group=hour(dateTime))) +
+  geom_density() + 
+  scale_colour_gradientn(colours=cmr$seasons) +
+  facet_grid(depthCat~sim)
+
+
+p <- velocity.df %>%
+  filter(sim==4) %>%
+  filter(ID %% 20 == 0) %>%
+  mutate(depthCat=if_else(depth<=5, "<= 5m", "> 5m")) %>%
+  ggplot() +
+  geom_sf(data=mesh.fp, fill=NA, colour="grey30") +
+  geom_path(aes(x, y, group=ID, colour=downloch), alpha=0.25, size=0.1) + 
+  scale_colour_gradient2("Direction", limits=c(-1,1), 
+                         breaks=c(-1,1), labels=c("uploch", "downloch")) +
+  facet_wrap(~depthCat) +
+  ggtitle("High resolution, 1h, medium lice speed")
+ggsave("figs/direction_map_sim4.png", p, width=8, height=5, dpi=300)
+
+p <- velocity.df %>%
+  filter(sim==3) %>%
+  filter(ID %% 20 == 0) %>%
+  mutate(depthCat=if_else(depth<=5, "<= 5m", "> 5m")) %>%
+  ggplot() +
+  geom_sf(data=mesh.fp, fill=NA, colour="grey30") +
+  geom_path(aes(x, y, group=ID, colour=downloch), alpha=0.25, size=0.1) + 
+  scale_colour_gradient2("Direction", limits=c(-1,1), 
+                         breaks=c(-1,1), labels=c("uploch", "downloch")) +
+  facet_wrap(~depthCat) +
+  ggtitle("High resolution, 1h, passive lice")
+ggsave("figs/direction_map_sim3.png", p, width=8, height=5, dpi=300)
+
+p <- velocity.df %>%
+  filter(ID %% 20 == 0) %>%
+  ggplot() +
+  geom_sf(data=mesh.fp, fill=NA, colour="grey30") +
+  geom_path(aes(x, y, group=ID, colour=downloch), alpha=0.25, size=0.1) + 
+  scale_colour_gradient2("Direction", limits=c(-1,1), 
+                         breaks=c(-1,1), labels=c("uploch", "downloch")) +
+  facet_wrap(~sim) +
+  ggtitle("High resolution, 1h, passive (3) or medium (4) lice speed")
+ggsave("figs/direction_map.png", p, width=8, height=5, dpi=300)
