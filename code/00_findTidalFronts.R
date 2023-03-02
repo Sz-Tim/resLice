98b26552c3146944ffbc356cf60fd5601aee8997
Sz-Tim <tim.szewczyk@gmail.com>
2023-03-02 08:54:50 +0000

    site profiles

--- code/00_findTidalFronts.R
+++ code/00_findTidalFronts.R
@@ -4,8 +4,8 @@ library(tidyverse); library(ncdf4); library(sf); library(glue); library(lubridat
 library(gganimate); theme_set(theme_bw())
 
 
-identify_pulse <- F
-make_gif <- T
+identify_pulse <- T
+make_gif <- F
 dirs <- list(
   proj=getwd(),
   mesh="D:/hydroOut/",
@@ -15,6 +15,8 @@ dirs <- list(
   WeStCOMS2_5min="D:/hydroOut/WeStCOMS2/Archive_5min/orig"
 )
 mesh.fp <- st_read("data/linnhe_mesh_footprint.gpkg")
+site.i <- read_csv("data/fishFarmSites_meshLocs.csv")
+
 
 par.df <- expand_grid(mesh=c("WeStCOMS2", "linnhe"),
                       tRes=c("1h", "5min")) %>%
@@ -41,45 +43,54 @@ for(j in 1:nrow(par.df)) {
       st_as_sf(coords=c("V1", "V2"), crs=27700) %>%
       mutate(i=row_number(),
              inPulseArea=st_is_within_distance(., pulseArea.sf, 50, sparse=F)[,1])
+    site.j <- site.i %>% filter(grepl(par.df$mesh[j], mesh))
+    
+    # Sigma layers and levels
+    sigLay <- ncvar_get(nc_open(dir(dirs[[paste(par.df$mesh[j], par.df$tRes[j], sep="_")]], full.names=T)[1]),
+                        "siglay")[1,]
+    sigLev <- ncvar_get(nc_open(dir(dirs[[paste(par.df$mesh[j], par.df$tRes[j], sep="_")]], full.names=T)[1]),
+                        "siglev")[1,]
+    
+    # Fish farm site profiles
+    for(i in 1:nrow(site.j)) {
+      nodes.ij <- unlist(select(site.j, starts_with("trinode"))[i,])
+      salProf.mx <- dir(dirs[[paste(par.df$mesh[j], par.df$tRes[j], sep="_")]], full.names=T) %>%
+        map(~ncvar_get(nc_open(.x), "salinity")[nodes.ij,,]) %>%
+        map(~apply(.x, 2:3, mean, na.rm=T)) %>%
+        do.call('cbind', .)
+      salProf.df <- expand_grid(time=1:ncol(salProf.mx), sigLayer=sigLay) %>% 
+        mutate(salinity=c(salProf.mx),
+               timeStart=as_datetime("2021-11-01 00:00:00") + (time-1)*ifelse(par.df$tRes[j]=="1h", 60, 5)*60,
+               timeEnd=timeStart + ifelse(par.df$tRes[j]=="1h", 60, 5)*60,
+               depth=site.j$depth[i]*sigLayer,
+               sigLevTop=rep(sigLev[1:length(sigLay)], ncol(salProf.mx)),
+               sigLevBottom=rep(sigLev[2:length(sigLev)], ncol(salProf.mx)),
+               depthTop=site.j$depth[i]*sigLevTop,
+               depthBottom=site.j$depth[i]*sigLevBottom) %>%
+        filter(timeEnd <= "2021-11-08 00:00:00")
+      write_csv(salProf.df, glue("out/salinity_profile_{site.j$site[i]}_{par.df$mesh[j]}_{par.df$tRes[j]}.csv"))
+      p <- ggplot(salProf.df, aes(fill=salinity)) +
+        geom_rect(aes(xmin=timeStart, xmax=timeEnd, ymin=depthBottom, ymax=depthTop)) + 
+        scale_fill_distiller(palette="Blues", direction=1, limits=c(10, 33)) +
+        scale_y_continuous("Depth (m)") +
+        scale_x_datetime("")
+      ggsave(glue("figs/mesh/salinity_profile_{site.j$site[i]}_{par.df$mesh[j]}_{par.df$tRes[j]}.png"), p,
+             width=7, height=5, dpi=300)
+      # p <- salProf.df %>%
+      #   filter(timeEnd <= "2021-11-03 00:00:00") %>%
+      #   ggplot(aes(fill=salinity)) +
+      #   geom_rect(aes(xmin=timeStart, xmax=timeEnd, ymin=depthBottom, ymax=depthTop)) + 
+      #   scale_fill_distiller(palette="Blues", direction=1, limits=c(10, 33)) +
+      #   scale_y_continuous("Depth (m)") +
+      #   scale_x_datetime("")
+      # ggsave(glue("figs/mesh/salinity_profile_2days_{site.j$site[i]}_{par.df$mesh[j]}_{par.df$tRes[j]}.png"), p,
+      #        width=7, height=5, dpi=300)
+    }
     
     # Find extreme salinity change across each time step
     salSurf.mx <- map(dir(dirs[[paste(par.df$mesh[j], par.df$tRes[j], sep="_")]], full.names=T), 
                       ~ncvar_get(nc_open(.x), "salinity")[,1,]) %>%
       do.call('cbind', .)
-    salProf.mx <- map(dir(dirs[[paste(par.df$mesh[j], par.df$tRes[j], sep="_")]], full.names=T), 
-                      ~ncvar_get(nc_open(.x), "salinity")[ifelse(par.df$mesh[j]=="WeStCOMS2", 71462, 12464),,]) %>%
-      do.call('cbind', .)
-    sigLay <- ncvar_get(nc_open(dir(dirs[[paste(par.df$mesh[j], par.df$tRes[j], sep="_")]], full.names=T)[1]),
-                        "siglay")[1,]
-    sigLev <- ncvar_get(nc_open(dir(dirs[[paste(par.df$mesh[j], par.df$tRes[j], sep="_")]], full.names=T)[1]),
-                        "siglev")[1,]
-    salProf.df <- expand_grid(time=1:ncol(salProf.mx), sigLayer=sigLay) %>% 
-      mutate(salinity=c(salProf.mx),
-             timeStart=as_datetime("2021-11-01 00:00:00") + (time-1)*ifelse(par.df$tRes[j]=="1h", 60, 5)*60,
-             timeEnd=timeStart + ifelse(par.df$tRes[j]=="1h", 60, 5)*60,
-             depth=60*sigLayer,
-             sigLevTop=rep(sigLev[1:length(sigLay)], ncol(salProf.mx)),
-             sigLevBottom=rep(sigLev[2:length(sigLev)], ncol(salProf.mx)),
-             depthTop=60*sigLevTop,
-             depthBottom=60*sigLevBottom) %>%
-      filter(timeEnd <= "2021-11-08 00:00:00")
-    write_csv(salProf.df, glue("out/salinity_profile_{par.df$mesh[j]}_{par.df$tRes[j]}.csv"))
-    p <- ggplot(salProf.df, aes(fill=salinity)) +
-      geom_rect(aes(xmin=timeStart, xmax=timeEnd, ymin=depthBottom, ymax=depthTop)) + 
-      scale_fill_distiller(palette="Blues", direction=1, limits=c(17, 32)) +
-      scale_y_continuous("Depth (m)") +
-      scale_x_datetime("")
-    ggsave(glue("figs/mesh/salinity_profile_{par.df$mesh[j]}_{par.df$tRes[j]}.png"), p,
-           width=7, height=5, dpi=300)
-    p <- salProf.df %>%
-      filter(timeEnd <= "2021-11-03 00:00:00") %>%
-      ggplot(aes(fill=salinity)) +
-      geom_rect(aes(xmin=timeStart, xmax=timeEnd, ymin=depthBottom, ymax=depthTop)) + 
-      scale_fill_distiller(palette="Blues", direction=1, limits=c(17, 32)) +
-      scale_y_continuous("Depth (m)") +
-      scale_x_datetime("")
-    ggsave(glue("figs/mesh/salinity_profile_2days_{par.df$mesh[j]}_{par.df$tRes[j]}.png"), p,
-           width=7, height=5, dpi=300)
     deltaSal <- salSurf.mx[,2:ncol(salSurf.mx)] - salSurf.mx[,1:(ncol(salSurf.mx)-1)]
     extremes.i <- map(1:ncol(deltaSal), ~which(deltaSal[,.x] <= par.df$thresh_deltaSal[j] &
                                                  deltaSal[,.x] < quantile(deltaSal[,.x], 0.01)))
